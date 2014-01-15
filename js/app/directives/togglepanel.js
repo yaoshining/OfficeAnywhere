68c922776497f268b41ee90ed26b713f383744e8
yao <ysnlvlqw@msn.com>
2014-01-15 17:42:59 +0800

    Add toggle panel

--- js/app/directives/togglepanel.js
+++ js/app/directives/togglepanel.js
@@ -2,25 +2,27 @@
  * Created by 世宁 on 14-1-15.
  */
 define(['modules/App','factories/TogglePanel'],function(app){
-    app.directive('togglepanel',function(TogglePanel){
+    app.directive('togglepanel',function(TogglePanel,$timeout){
         return {
             restrict: "A",
             link: function(scope,element){
-                element.find(".toggleButton").toggle(function(){
-                    var button = this;
-                    element.animate({
-                        right: "0px"
-                    },1000,function(){
-                        $(button).find(".button-name").text("关闭");
+                $timeout(function(){
+                    element.find(".toggleButton").toggle(function(){
+                        var button = this;
+                        element.animate({
+                            right: "0px"
+                        },1000,function(){
+                            $(button).find(".button-name").text("关闭");
+                        });
+                    },function(){
+                        var button = this;
+                        element.animate({
+                            right: "-240px"
+                        },1000,function(){
+                            $(button).find(".button-name").text("打开");
+                        });
                     });
-                },function(){
-                    var button = this;
-                    element.animate({
-                        right: "-240px"
-                    },1000,function(){
-                        $(button).find(".button-name").text("打开");
-                    });
-                });
+                },500);
             },
             controller: function($scope){
                 var togglePanel = TogglePanel.query(function(){
