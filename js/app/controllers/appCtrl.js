6c56c59150de8a3ef701c7814c79b2f90e648e54
yao <ysnlvlqw@msn.com>
2014-01-17 15:05:38 +0800

    Update style

--- js/app/controllers/appCtrl.js
+++ js/app/controllers/appCtrl.js
@@ -1,12 +1,8 @@
 /**
  * Created by 世宁 on 14-1-3.
  */
-define(['angular','modules/App','directives/layout','directives/shader','factories/UserInfo'] , function (angular,app) {
+define(['angular','modules/App','directives/layout','directives/shader'] , function (angular,app) {
     app.controller('appCtrl' ,'UserInfo', function ($scope,$rootScope,UserInfo) {
-        console.log(UserInfo);
-        var user = UserInfo.query(function(){
-            alert(1);
-            console.log(user);
-        });
+
     })
 });
--- js/app/controllers/centerCtrl.js
+++ js/app/controllers/centerCtrl.js
@@ -1,8 +1,11 @@
 /**
  * Created by 世宁 on 14-1-3.
  */
-define(['angular','modules/App','services/centerService'] , function (angular,app) {
-    app.controller('centerCtrl' , function ($scope,centerService) {
+define(['angular','modules/App','services/centerService','factories/UserInfo'] , function (angular,app) {
+    app.controller('centerCtrl' , function ($scope,centerService,UserInfo,$rootScope) {
+        var userInfo = UserInfo.query(function(){
+            $rootScope.userInfo = userInfo;
+        });
         $scope.template = centerService.template;
         $scope.$on("center.update",function(event,template,apply){
             $scope.template = template;
